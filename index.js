c1c2657461041211f6372ae01dc16d4b2ca4e7f8
Shama Tabassum <shamatabassum071@gmail.com>
2023-07-13 00:00:17 +0500

    dropdown added

--- index.js
+++ index.js
@@ -18,32 +18,101 @@ let list = document.querySelector('#list');
 } 
 window.Menu = Menu;
 // function for dropdown menu 
-const loan = document.getElementById('loan');
-const loanText = document.getElementById('loan-text');
-const downArrow = document.getElementById('down-arrow');
-
-    downArrow && loanText.addEventListener('mouseover', () => {
-      loan.classList.add('block');
-      loan.classList.remove('hidden');
-    });
-    downArrow && loanText.addEventListener('mouseout', () => {
-      loan.classList.remove('block');
-      loan.classList.add('hidden');
-    });
-// function for dropdown menu 1
-const loan1 = document.getElementById('loan1');
-const loanText1 = document.getElementById('loan-text1');
+const dropdownList1 = document.getElementById('dropdown-list1');
+const dropdownText1 = document.getElementById('dropdown-text1');
 const downArrow1 = document.getElementById('down-arrow1');
 
-downArrow1 && loanText1.addEventListener('mouseover', () => {
-  loan1.classList.add('block');
-  loan1.classList.remove('hidden');
+downArrow1 && dropdownText1.addEventListener('mouseover', () => {
+  dropdownList1.classList.add('block');
+  dropdownList1.classList.remove('hidden');
 });
-downArrow1 && loanText1.addEventListener('mouseout', () => {
-  loan1.classList.remove('block');
-  loan1.classList.add('hidden');
+downArrow1 && dropdownText1.addEventListener('mouseout', () => {
+  dropdownList1.classList.remove('block');
+  dropdownList1.classList.add('hidden');
 });
 
+// function for dropdown menu 2
+const dropdownList2 = document.getElementById('dropdown-list2');
+const dropdownText2 = document.getElementById('dropdown-text2');
+const downArrow2 = document.getElementById('down-arrow2');
+
+downArrow2 && dropdownText2.addEventListener('mouseover', () => {
+  dropdownList2.classList.add('block');
+  dropdownList2.classList.remove('hidden');
+});
+downArrow2 && dropdownText2.addEventListener('mouseout', () => {
+  dropdownList2.classList.remove('block');
+  dropdownList2.classList.add('hidden');
+});
+
+
+// function for dropdown menu 2-1
+const dropdownList21 = document.getElementById('dropdown-list2-1');
+const dropdownText21 = document.getElementById('dropdown-text2-1');
+const downArrow21 = document.getElementById('down-arrow2-1');
+
+downArrow21 && dropdownText21.addEventListener('mouseover', () => {
+  dropdownList21.classList.add('block');
+  dropdownList21.classList.remove('hidden');
+});
+downArrow21 && dropdownText21.addEventListener('mouseout', () => {
+  dropdownList21.classList.remove('block');
+  dropdownList21.classList.add('hidden');
+});
+// function for dropdown menu 2-2
+const dropdownList22 = document.getElementById('dropdown-list2-2');
+const dropdownText22 = document.getElementById('dropdown-text2-2');
+const downArrow22 = document.getElementById('down-arrow2-2');
+
+downArrow22 && dropdownText22.addEventListener('mouseover', () => {
+  dropdownList22.classList.add('block');
+  dropdownList22.classList.remove('hidden');
+});
+downArrow22 && dropdownText22.addEventListener('mouseout', () => {
+  dropdownList22.classList.remove('block');
+  dropdownList22.classList.add('hidden');
+});
+// function for dropdown menu 2-3
+const dropdownList23 = document.getElementById('dropdown-list2-3');
+const dropdownText23 = document.getElementById('dropdown-text2-3');
+const downArrow23 = document.getElementById('down-arrow2-3');
+
+downArrow23 && dropdownText23.addEventListener('mouseover', () => {
+  dropdownList23.classList.add('block');
+  dropdownList23.classList.remove('hidden');
+});
+downArrow23 && dropdownText23.addEventListener('mouseout', () => {
+  dropdownList23.classList.remove('block');
+  dropdownList23.classList.add('hidden');
+});
+
+// function for dropdown menu 2-4
+const dropdownList24 = document.getElementById('dropdown-list2-4');
+const dropdownText24 = document.getElementById('dropdown-text2-4');
+const downArrow24 = document.getElementById('down-arrow2-4');
+
+downArrow24 && dropdownText24.addEventListener('mouseover', () => {
+  dropdownList24.classList.add('block');
+  dropdownList24.classList.remove('hidden');
+});
+downArrow24 && dropdownText24.addEventListener('mouseout', () => {
+  dropdownList24.classList.remove('block');
+  dropdownList24.classList.add('hidden');
+});
+
+// function for dropdown menu 3
+const dropdownList3 = document.getElementById('dropdown-list3');
+const dropdownText3 = document.getElementById('dropdown-text3');
+const downArrow3 = document.getElementById('down-arrow3');
+
+downArrow3 && dropdownText3.addEventListener('mouseover', () => {
+  dropdownList3.classList.add('block');
+  dropdownList3.classList.remove('hidden');
+});
+downArrow3 && dropdownText3.addEventListener('mouseout', () => {
+  dropdownList3.classList.remove('block');
+  dropdownList3.classList.add('hidden');
+});
     // ......
     // function to Calculate Business Loan 
     var startDate = document.getElementById("start-date");
